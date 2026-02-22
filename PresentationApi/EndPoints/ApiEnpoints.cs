1440ecd43ff91dcfdbbf7d2ad243ffdbdbd89cdb
THEOD0R1 <squirrelnub@gmail.com>
2026-02-22 22:06:39 +0100

    Add API endpoint mapping and update project structure
    - Registered CORS and validation services in Program.cs.
    - Added a ground structure for MapApiEndpoints.cs.

---
+++ PresentationApi/EndPoints/ApiEnpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace PresentationApi.EndPoints;
+
+public static class ApiEnpoints
+{
+    public static IEndpointRouteBuilder MapApiEndpoints(this IEndpointRouteBuilder app)
+    {
+        var api = app.MapGroup("/api");
+
+        return app;
+    }
+}
--- PresentationApi/Program.cs
+++ PresentationApi/Program.cs
@@ -1,11 +1,18 @@
+using PresentationApi.EndPoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
+builder.Services.AddValidation();
 
 var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.MapApiEndpoints();
 
 app.Run();
