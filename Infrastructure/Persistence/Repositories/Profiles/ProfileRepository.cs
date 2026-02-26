48bcbcf2892c84b43e91c341eaeb4765a6d9c916
THEOD0R1 <squirrelnub@gmail.com>
2026-02-26 11:43:01 +0100

    Add ProfileRepository with GetByEmailAsync method

---
+++ Infrastructure/Persistence/Repositories/Profiles/ProfileRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Common.Base;
+using Domain.Models.ValueObjects;
+using Domain.Profiles;
+using Domain.Profiles.Repositories;
+using Infrastructure.Persistence.Contexts;
+using Infrastructure.Persistence.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Profiles;
+
+public sealed class ProfileRepository(CourseOnlineDbContext context) : RepositoryBase<Profile, Guid, ProfileEntity, CourseOnlineDbContext>(context), IProfileRepository
+{
+    public async Task<Profile?> GetByEmailAsync(Email email, CancellationToken ct)
+    {
+        var entity = await Set
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Email == email.Value, ct);
+
+        return entity is null ? default : ToModel(entity);
+    }
+
+    protected override ProfileEntity ToEntity(Profile model)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Profile ToModel(ProfileEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
